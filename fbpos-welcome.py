4d27aa9bde6cfd9a65dbbcaabb922b09d6363280
Filma Bem <filmab.emtv2@gmail.com>
2023-05-24 21:07:51 +0100

    Added Timeshift button

--- fbpos-welcome.py
+++ fbpos-welcome.py
@@ -17,15 +17,20 @@ welcome._set_appearance_mode('System')
 def system_settings():
     os.system('systemsettings')
 
+def launch_timeshift():
+    os.system('pkexec timeshift-gtk')
 
 
 # Application Widgets
 
 welcome_label = customtkinter.CTkLabel(welcome, text='Welcome to FBP OS. We will help you to configure your new system')
-welcome_label.pack()
+welcome_label.pack(padx=20, pady=10)
 
 settings_button = customtkinter.CTkButton(welcome, text='Settings', command=system_settings)
-settings_button.pack()
+settings_button.pack(padx=20, pady=10)
+
+timeshift = customtkinter.CTkButton(welcome, text='Backup and restore', command=launch_timeshift)
+timeshift.pack(padx=20, pady=10)
 
 # Launch Application
 
