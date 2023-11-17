eff9d1f2ed2d02e3bbbec9cbd8dc306127b3ba65
Filma Bem <filmab.emtv2@gmail.com>
2023-11-17 19:14:48 +0000

    Removed the check for yay since it's not needed

--- fbpos-welcome.py
+++ fbpos-welcome.py
@@ -14,6 +14,18 @@ welcome.geometry('500x600')
 welcome.title('FBP OS Welcome')
 welcome._set_appearance_mode('System')
 
+settings_frame = customtkinter.CTkFrame(welcome)
+settings_label = customtkinter.CTkLabel(settings_frame, text='Tweak settings')
+settings_label.pack(padx=20, pady=10)
+
+drivers_frame = customtkinter.CTkFrame(welcome)
+drivers_label = customtkinter.CTkLabel(drivers_frame, text="Install Drivers")
+drivers_label.pack(padx=20, pady=10)
+
+software_frame = customtkinter.CTkFrame(welcome)
+software_label = customtkinter.CTkLabel(software_frame, text='Install software packs')
+software_label.pack(padx=20, pady=10)
+
 # Software stores
 
 discover = os.path.exists('usr/bin/plasma-discover')
@@ -27,26 +39,6 @@ yay_bin = '/usr/bin/yay'
 # Distro ID
 
 distro_id = ''
-
-# Check for yay and install if it's not installed
-
-if not os.path.exists(yay_bin):
-    print('yay not found')
-    print('Checking if you are running FBP OS or Arch Linux')
-    if distro_id == 'fbpos':
-        print('Installing yay on FBP OS...')
-        os.system('pkexec pacman -S yay')
-    elif distro_id == 'arch':
-        print('Installing yay on Arch Linux')
-        os.system('pkexec pacman -S go base-devel pacman-contrib git')
-        os.system('git clone https://aur.archlinux.org/yay.git')
-        os.system('cd ./yay/')
-        os.system('updpkgsums')
-        os.system('makepkg --install')
-    else:
-        print('Not on FBP OS or Arch Linux')
-
-    
 
 # Custom messagebox function
 
@@ -158,40 +150,46 @@ def sure_install_hybrid():
 welcome_label = customtkinter.CTkLabel(welcome, text='Welcome to FBP OS. We will help you to configure your new system')
 welcome_label.pack(padx=20, pady=10)
 
-settings_button = customtkinter.CTkButton(welcome, text='Settings', command=system_settings)
+# Frame Stacking
+
+settings_frame.pack(padx=20, pady=10)
+drivers_frame.pack(padx=20, pady=10)
+software_frame.pack(padx=20, pady=10)
+
+settings_button = customtkinter.CTkButton(settings_frame, text='Settings', command=system_settings)
 settings_button.pack(padx=20, pady=10)
 
-fix_pacman_keys = customtkinter.CTkButton(welcome, text='Fix Pacman Keys', command=fix_pacman)
+fix_pacman_keys = customtkinter.CTkButton(settings_frame, text='Fix Pacman Keys', command=fix_pacman)
 fix_pacman_keys.pack(padx=20, pady=10)
 
-timeshift = customtkinter.CTkButton(welcome, text='Backup and restore', command=launch_timeshift)
+timeshift = customtkinter.CTkButton(settings_frame, text='Backup and restore', command=launch_timeshift)
 timeshift.pack(padx=20, pady=10)
 
-nvidia_driver = customtkinter.CTkButton(welcome, text='Install Nvidia Driver (nvidia users only)', command=sure_install_nvidia_driver)
+nvidia_driver = customtkinter.CTkButton(drivers_frame, text='Install Nvidia Driver (nvidia users only)', command=sure_install_nvidia_driver)
 nvidia_driver.pack(padx=20, pady=10)
 
-bumblebee = customtkinter.CTkButton(welcome, text='Install Nvidia Optimus (Hybrid graphics only)', command=sure_install_hybrid)
+bumblebee = customtkinter.CTkButton(drivers_frame, text='Install Nvidia Optimus (Hybrid graphics only)', command=sure_install_hybrid)
 bumblebee.pack(padx=20, pady=10)
 
-software_center = customtkinter.CTkButton(welcome, text='Software Center', command=launch_software_center)
+software_center = customtkinter.CTkButton(settings_frame, text='Software Center', command=launch_software_center)
 software_center.pack(padx=20, pady=10)
 
-install_game_launchers = customtkinter.CTkButton(welcome, text='Install some game launchers', command=install_games)
+install_game_launchers = customtkinter.CTkButton(software_frame, text='Install some game launchers', command=install_games)
 install_game_launchers.pack(padx=20, pady=10)
 
-office_suite = customtkinter.CTkButton(welcome, text='Install Office Suite', command=install_office_suite)
+office_suite = customtkinter.CTkButton(software_frame, text='Install Office Suite', command=install_office_suite)
 office_suite.pack(padx=20, pady=10)
 
-security_software = customtkinter.CTkButton(welcome, text='Install Account Security Software', command=install_security_software)
+security_software = customtkinter.CTkButton(software_frame, text='Install Account Security Software', command=install_security_software)
 security_software.pack(padx=20, pady=10)
 
-rustdesk = customtkinter.CTkButton(welcome, text='Install Remote Control Software', command=remote_control_software)
+rustdesk = customtkinter.CTkButton(software_frame, text='Install Remote Control Software', command=remote_control_software)
 rustdesk.pack(padx=20, pady=10)
 
-vmware_addons = customtkinter.CTkButton(welcome, text='Install Vmware Addons (Vmware virtual machines only)', command=install_vmware_guest_addons)
+vmware_addons = customtkinter.CTkButton(drivers_frame, text='Install Vmware Addons (Vmware virtual machines only)', command=install_vmware_guest_addons)
 vmware_addons.pack(padx=20, pady=10)
 
-virtualbox_addons = customtkinter.CTkButton(welcome, text='Install Virtualbox Addons (Virtualbox virtual machines only)', command=install_virtualbox_addons)
+virtualbox_addons = customtkinter.CTkButton(drivers_frame, text='Install Virtualbox Addons (Virtualbox virtual machines only)', command=install_virtualbox_addons)
 virtualbox_addons.pack(padx=20, pady=10)
 
 # Launch Application
