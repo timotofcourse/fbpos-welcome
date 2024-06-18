c505f78b5f3ef1937701eb2707e95b2922f903dd
Timot <filmab.emtv2@gmail.com>
2024-06-18 02:34:41 +0100

    Removed unused files

--- find_binaries.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-discover = os.path.exists('usr/bin/plasma-discover')
-software = os.path.exists('usr/bin/gnome-software')
-mint = os.path.exists('usr/bin/mintinstall')
-
-yay_bin = '/usr/bin/yay'
